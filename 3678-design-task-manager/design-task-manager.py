368c23499a3067cd1d7afcddad333c0c05d87cdb
Suresh Surabathula <sureshsurabathula69@gmail.com>
2025-09-18 01:54:00 -0400

    Time: 2180 ms (5.28%) | Memory: 98.7 MB (93.96%) - LeetSync

---
+++ 3678-design-task-manager/design-task-manager.py
@@ -0,0 +1,43 @@
+import bisect
+
+class TaskManager:
+    def __init__(self, tasks):
+        self.mp = {}  # taskId -> (userId, priority)
+        self.st = []  # sorted list of (priority, taskId)
+
+        for userId, taskId, priority in tasks:
+            self.mp[taskId] = (userId, priority)
+            bisect.insort(self.st, (priority, taskId))
+
+    def add(self, userId, taskId, priority):
+        self.mp[taskId] = (userId, priority)
+        bisect.insort(self.st, (priority, taskId))
+
+    def edit(self, taskId, newPriority):
+        userId, oldPriority = self.mp[taskId]
+        idx = bisect.bisect_left(self.st, (oldPriority, taskId))
+        self.st.pop(idx)
+        self.mp[taskId] = (userId, newPriority)
+        bisect.insort(self.st, (newPriority, taskId))
+
+    def rmv(self, taskId):
+        userId, priority = self.mp[taskId]
+        idx = bisect.bisect_left(self.st, (priority, taskId))
+        self.st.pop(idx)
+        del self.mp[taskId]
+
+    def execTop(self):
+        if not self.mp:
+            return -1
+        priority, taskId = self.st.pop()  # highest priority
+        userId, _ = self.mp[taskId]
+        del self.mp[taskId]
+        return userId
+
+
+# Your TaskManager object will be instantiated and called as such:
+# obj = TaskManager(tasks)
+# obj.add(userId,taskId,priority)
+# obj.edit(taskId,newPriority)
+# obj.rmv(taskId)
+# param_4 = obj.execTop()
